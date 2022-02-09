7facd2236a554fa3161d5b53cf9142f020a70e7e
nao <nao_h13@hotmail.com>
2022-02-10 01:04:24 +0900

    useRefの設定 と useRefの設定

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './Tooltip.css';
 
 const Tooltip = ({ children, content, location }) => {
+    const tooltipEl = useRef();
     const [ show, setShow ] = useState( false );
 
+    const clickOutside = (e) => {
+        if (!tooltipEl.current.contains(e.target)) setShow(false);
+      };
+
+    useEffect( () => {
+        document.addEventListener('click', clickOutside );
+        return () => {
+            document.removeEventListener('click', clickOutside );
+        }
+    }, []);
+
     return (
-        <div className="container">
+        <div className="container" ref={tooltipEl}>
             <div
                 onClick={() => setShow( !show )}
                 >
